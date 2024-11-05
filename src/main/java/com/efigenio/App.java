b79e99fc69e791a806c02fcae0e9969197b9d97c
Davishisaki <>
2024-11-05 17:35:37 -0300

    oi

--- src/main/java/com/efigenio/App.java
+++ src/main/java/com/efigenio/App.java
@@ -5,7 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
+ 
 import java.io.IOException;
 
 public class App extends Application {
